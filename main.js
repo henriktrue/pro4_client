26665b8787244e463e08317f40a98e6ad1ef9044
Henrik Truelsen <31506722+henriktrue@users.noreply.github.com>
2018-05-06 19:15:58 +0200

    opdateret til nye kerne
    winning

--- main.js
+++ main.js
@@ -8,16 +8,37 @@ var duty3 = 150;
 var duty4 = 150;
 var duty5 = 150;
 
-//setting period for motor 5
-motors 1-4 is set in the device tree overlays
-fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P9_14.15/period", "1520000");
+//fs.writeFileSync("config-pin P9.22 pwm");
+//fs.writeFileSync("config-pin P9.21 pwm");
+//fs.writeFileSync("config-pin P9.14 pwm");
+//fs.writeFileSync("config-pin P8.19 pwm");
+//fs.writeFileSync("config-pin P8.13 pwm");
 
-//setting polarity for motors
-fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P9_14.15/polarity", "0");
-fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P9_16.16/polarity", "0");
-fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P8_13.18/polarity", "0");
-fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P8_19.19/polarity", "0");
-fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P9_42.17/polarity", "0");
+//P9.22
+fs.writeFileSync("/sys/class/pwm/pwmchip0/export", "0"); 
+fs.writeFileSync("/sys/class/pwm/pwm-0:0/period", "20000000"); 	
+fs.writeFileSync("/sys/class/pwm/pwm-0:0/polarity", "0"); 	
+
+//P9.21
+fs.writeFileSync("/sys/class/pwm/pwmchip1/export", "0"); 	
+fs.writeFileSync("/sys/class/pwm/pwm-1:0/period", "20000000"); 	
+fs.writeFileSync("/sys/class/pwm/pwm-1:0/polarity", "0"); 	
+
+//P9.14
+fs.writeFileSync("/sys/class/pwm/pwmchip3/export", "0"); 	
+fs.writeFileSync("/sys/class/pwm/pwm-3:0/period", "20000000"); 	
+fs.writeFileSync("/sys/class/pwm/pwm-3:0/polarity", "0"); 	
+
+//P8.19
+fs.writeFileSync("/sys/class/pwm/pwmchip5/export", "0"); 	
+fs.writeFileSync("/sys/class/pwm/pwm-5:0/period", "20000000"); 	
+fs.writeFileSync("/sys/class/pwm/pwm-5:0/polarity", "0"); 	
+
+//P8.13
+fs.writeFileSync("/sys/class/pwm/pwmchip6/export", "0"); 	
+fs.writeFileSync("/sys/class/pwm/pwm-6:0/period", "20000000"); 	
+fs.writeFileSync("/sys/class/pwm/pwm-6:0/polarity", "0"); 	
+
 
 //motor1 left
 socket.on('m1_l', function(){
@@ -26,8 +47,7 @@ socket.on('m1_l', function(){
 		var m1 = duty1++;
 		var m1 = duty1*10000;
 		console.log(m1);
-		fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P9_42.17/duty", m1);
-
+		fs.writeFileSync("/sys/class/pwm/pwm-0:0/duty_cycle", m1);
 	}
 });
 
@@ -38,7 +58,7 @@ socket.on('m1_r', function(){
 		var m1 = duty1--;
 		var m1 = duty1*10000;
 		console.log(m1);
-		fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P9_14.15/duty", m1);
+		fs.writeFileSync("/sys/class/pwm/pwm-0:0/duty_cycle", m1);
 	}
 });
 
@@ -49,7 +69,7 @@ socket.on('m2_l', function(){
 		var m2 = duty2++;
 		var m2 = duty2*10000;
 		console.log(m2);
-		fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P9_16.16/duty", m2);
+		fs.writeFileSync("/sys/class/pwm/pwm-1:0/duty_cycle", m2);
 	}
 });
 
@@ -60,7 +80,7 @@ socket.on('m2_r', function(){
 		var m2 = duty2--;
 		var m2 = duty2*10000;
 		console.log(m2);
-		fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P9_16.16/duty", m2);
+		fs.writeFileSync("/sys/class/pwm/pwm-1:0/duty_cycle", m2);
 	}
 });
 
@@ -71,7 +91,7 @@ socket.on('m3_l', function(){
 		var m3 = duty3++;
 		var m3 = duty3*10000;
 		console.log(m3);
-		fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P8_13.18/duty", m3);
+		fs.writeFileSync("/sys/class/pwm/pwm-3:0/duty_cycle", m3);
 	}
 });
 
@@ -83,7 +103,7 @@ socket.on('m3_r', function(){
 		var m3 = duty3*10000;
 		console.log(m3);
 		console.log(m3);
-		fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P8_13.18/duty", m3);
+		fs.writeFileSync("/sys/class/pwm/pwm-3:0/duty_cycle", m3);
 	}
 });
 
@@ -94,7 +114,7 @@ socket.on('m4_l', function(){
 		var m4 = duty4++;
 		var m4 = duty4*10000;
 		console.log(m4);
-		fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P8_19.19/duty", m4);
+		fs.writeFileSync("/sys/class/pwm/pwm-5:0/duty_cycle", m4);
 	}
 });
 
@@ -105,7 +125,7 @@ socket.on('m4_r', function(){
 		var m4 = duty4--;
 		var m4 = duty4*10000;
 		console.log(m4);
-		fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P8_19.19/duty", m4);
+		fs.writeFileSync("/sys/class/pwm/pwm-5:0/duty_cycle", m4);
 	}
 });
 
@@ -116,7 +136,7 @@ socket.on('m5_l', function(){
 		var m5 = duty5++;
 		var m5 = duty5*10000;
 		console.log(m5);
-		fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P9_42.17/duty", m5);
+		fs.writeFileSync("/sys/class/pwm/pwm-6:0/duty_cycle", m5);
 	}
 });
 
@@ -127,6 +147,6 @@ socket.on('m5_r', function(){
 		var m5 = duty5--;
 		var m5 = duty5*10000;
 		console.log(m5);
-		fs.writeFileSync("/sys/devices/ocp.3/bs_pwm_test_P9_42.17/duty", m5);
+		fs.writeFileSync("/sys/class/pwm/pwm-6:0/duty_cycle", m5);
 	}
 });
