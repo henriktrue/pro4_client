6541cabb082acc9fed22aaa8afe9ec88c2f0f65e
root <root@beaglebone.localdomain>
2018-05-07 09:07:42 +0000

    added init script

---
+++ enable-pwm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+       # Connect the pins to the PWM
+        config-pin p9_14 pwm
+        config-pin p9_16 pwm
+        config-pin p9_42 pwm
+        config-pin p8_19 pwm
+        config-pin p8_13 pwm
+
+        # Now we are ready to enable the PWM chip
+        echo 0 > /sys/class/pwm/pwmchip0/export
+        echo 0 > /sys/class/pwm/pwmchip3/export
+        echo 1 > /sys/class/pwm/pwmchip3/export
+        echo 0 > /sys/class/pwm/pwmchip6/export
+        echo 1 > /sys/class/pwm/pwmchip6/export
+
+	echo "pwm setup complete"
+
+nodejs main
+
